7d5d331b6f97ed5eae84918d228c3abd7531d9bb
Sougata Modak <sougata.modak2020@gmail.com>
2025-11-17 14:21:24 +0530

    fix: ensure Providers.tsx committed

---
+++ src/components/layout/Providers.tsx
@@ -0,0 +1,11 @@
+﻿'use client';
+import { Toaster } from 'react-hot-toast';
+import { ThemeProvider } from 'next-themes';
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system">
+      {children}
+      <Toaster position="top-right" />
+    </ThemeProvider>
+  );
+}
